c85d6790fb94ec36f33e72d4ae49875269b2bf53
Diksha <dikshabuxani84745@gmail.com>
2024-02-26 23:45:11 +0530

    nav bar front page in progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
+import Dashboard from "./dashboard";
 
 export default function Home() {
   return (
-   <section className="px-4 py-6">
-      <div className="flex justify-between">
-        <h1>hireBot.fyi</h1>
-       
+   <section className="">
+      <div className="flex flex-col ">
+       <Dashboard/>
       </div>
     </section>
   )
